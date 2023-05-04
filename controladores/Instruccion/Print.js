349aca9ad577df3c9eb3ef07afb10bb9f6ffe1b2
KESM12 <3001920290101@ingenieria.usac.edu.gt>
2023-05-03 20:28:55 -0600

    Update Print.js

--- controladores/Instruccion/Print.js
+++ controladores/Instruccion/Print.js
@@ -1,6 +1,7 @@
 const procesarCadena = require("../Operaciones/procesarCadena")
 
 function Print(_instruccion, _ambito){
+    console.log(_instruccion, "instruccion print")
     const cadena = procesarCadena(_instruccion.expresion, _ambito).valor
     return cadena
 }
